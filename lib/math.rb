5e76aeaa494b10eaaab2823a243b81c91647ce8e
Ray Reyes <rayreyes5@gmail.com>
2019-09-03 22:21:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,20 +4,20 @@ end num1 additon num2
 
 def subtraction(num1, num2)
 
-end 
+end num1 subtraction num2
 
 def division(num1, num2)
 
-end
+end num1 division num2
 
 def multiplication(num1, num2)
 
-end
+end num1 multiplication num2
 
 def modulo(num1, num2)
 
-end
+end num1 modulo num2
 
 def square_root(num)
 
-end
+end num square_root
